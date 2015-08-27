102fffae3cf9c2592e30376297c9b07285812996
Dan Croak <dan@thoughtbot.com>
2015-08-26 21:38:57 -0700

    Rename

--- parity.go
+++ parity.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"os"
+
 	"github.com/codegangsta/cli"
 )
 
@@ -15,8 +17,11 @@ var (
 
 func main() {
 	app := cli.NewApp()
-	app.Name = ""
+	app.Name = "parity"
 	app.Usage = ""
 	app.Version = version
-	app.RunAndExitOnError()
+	app.Action = func(c *cli.Context) {
+		println("Hello", c.Args()[0])
+	}
+	app.Run(os.Args)
 }
